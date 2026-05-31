d2027b4b453c9ea2592a2a005606a00f1076b13d
shaderw0lf <55358751+shaderw0lf@users.noreply.github.com>
2026-05-31 15:58:28 +0200

    test: enabling captcha on email

--- routers/logregin.py
+++ routers/logregin.py
@@ -65,7 +65,7 @@ async def requestCode(request: Request):
     if not EmailProcessor.Validate(reciever):
         return Errors.InvalidEmail(timestamp() - t1)
 
-    inspector = FISH.cook(("email:" + reciever).encode("utf-8"))
+    inspector = FISH.cook(("email:" + reciever).encode()).decode()
     turtle = await CacheProcessor.Get(inspector, prefix="turtlelimiter:")
     if turtle is None:
         await CacheProcessor.Make(
