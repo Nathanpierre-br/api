90f27c4a187dd714afe6009990dea20cb6ec1a2c
shaderw0lf <55358751+shaderw0lf@users.noreply.github.com>
2026-05-31 16:01:28 +0200

    test: failed

--- routers/logregin.py
+++ routers/logregin.py
@@ -65,7 +65,7 @@ async def requestCode(request: Request):
     if not EmailProcessor.Validate(reciever):
         return Errors.InvalidEmail(timestamp() - t1)
 
-    inspector = FISH.cook(("email:" + reciever).encode()).decode()
+    """inspector = FISH.cook(("email:" + reciever).encode()).decode()
     turtle = await CacheProcessor.Get(inspector, prefix="turtlelimiter:")
     if turtle is None:
         await CacheProcessor.Make(
@@ -77,7 +77,7 @@ async def requestCode(request: Request):
         return Errors.VerificationRequired(
             Config.API_BASE_URL + "/api/v1/turtle/hello/email?inspector=" + inspector,
             timestamp() - t1,
-        )
+        )"""
 
     db = await Database().init()
     table = await db.get(table="VerificationCodes")
