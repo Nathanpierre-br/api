5a42cd501e76e9573f191bfb9aec35e2d35f33ee
alx0rr <anon.mail.al@proton.me>
2026-06-27 01:46:30 +0300

    add support reset password

--- routers/altteam.py
+++ routers/altteam.py
@@ -1,3 +1,11 @@
+from asyncio import to_thread as asyncio_thread
+
+from helpers.aquarium import Blake
+from helpers.config import Config
+from helpers.database.mongo import Database
+from helpers.processors.email import EmailProcessor
+
+
 from time import time as timestamp
 
 from fastapi import APIRouter, Request
@@ -6,12 +14,66 @@ from helpers.routers.cachable import CachableRoute
 from objects import Base, Errors
 
 
+from string import ascii_letters, digits
+import secrets
+
 altteam = APIRouter()
 altteam.route_class = CachableRoute
 
+WHO_HAVE_POWER_OF_GOD = [200, 201, 254, 555]
 
 
-@altteam.get("/g/s/altteam")
-@altteam.get("/x{ndcId}/s/altteam")
-async def mock(request: Request, ndcId: int = 0):
-    return Base.Answer()
+
+
+
+@altteam.get("/g/s/altteam/reset-password")
+async def support_reset_password(request: Request, ndcId: int = 0):
+    t1 = timestamp()
+    if not Config.ENABLE_EMAIL:
+        return Errors.PathUnderMaintenance(timestamp() - t1)
+    trigger_uid = request.state.session.get("uid")
+    db = await Database().init()
+    sensitive_table = db.get(table="Users")
+    user = await sensitive_table.find_one({"id": trigger_uid})
+    if not user or user.get("role", 0) not in WHO_HAVE_POWER_OF_GOD:
+        db.close()
+        return Errors.NotEnoughRights(timestamp() - t1)
+    try:
+        secret = ''.join(secrets.choice(ascii_letters + digits) for _ in range(12))
+        data = await request.json()
+        updateSecret = Blake(
+            data=secret,
+            key=Config.PASSWORD_SALT,
+            digest_size=64,
+        ).hash
+        email = data["email"]
+    except Exception:
+        return Errors.InvalidRequest(timestamp() - t1)
+    if not await EmailProcessor.Validate(email):
+        return Errors.InvalidEmail(timestamp() - t1)
+    table = db.get(table="Users")
+    await table.update_one({"email": email}, {"$set": {"passwordHash": updateSecret}})
+    db.close()
+
+    html = """<h3>Your AltAmino password has been reset by support.</h3><p>Your new password is:</p><h2>{{ PASSWORD }}</h2><p>Please log in using this password and change it as soon as possible in your account settings.</p><p>If you did not request this, please contact us immediately.</p><br><p>Thanks,<br>Team AltAmino</p>"""
+    text = "Your AltAmino password has been reset by support. Your new temporary password is: {{ PASSWORD }}. Please log in and change it as soon as possible in your account settings. If you did not request this, please contact us immediately."
+
+    html = html.replace("{{ PASSWORD }}", secret)
+    text = text.replace("{{ PASSWORD }}", secret)
+    subject = "Your AltAmino password has been reset"
+
+    try:
+        await asyncio_thread(
+            EmailProcessor.SendEmail,
+            receiver=email,
+            subject=subject,
+            html=html,
+            text=text,
+        )
+    except Exception as e:
+        print(e)
+        return Errors.MailError(timestamp() - t1)
+    return Base.Answer(
+        {},
+        spent_time=timestamp() - t1,
+    )
