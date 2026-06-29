f3b8ad3347d63fbec1287f0cb0f92702944f47e6
alx0rr <anon.mail.al@proton.me>
2026-06-29 06:15:23 +0300

    test altteamv5 debug

--- routers/altteam.py
+++ routers/altteam.py
@@ -41,14 +41,13 @@ async def get_altamino_team(request: Request):
             {"_id": 0, "id": 1, "role": 1, "tagList": 1},
         )
         global_members = await global_cursor.to_list(length=None)
-        print(global_members)
         if not global_members:
             return Base.Answer(spent_time=timestamp() - t1, userProfileList=[])
 
         global_ids = [m["id"] for m in global_members]
 
         local_cursor = local_table.find(
-            {"uid": {"$in": global_ids}},
+            {"id": {"$in": global_ids}},
             {
                 "_id": 0,
                 "uid": 1,
@@ -62,7 +61,6 @@ async def get_altamino_team(request: Request):
         )
         local_profiles = await local_cursor.to_list(length=None)
         local_by_uid = {p["uid"]: p for p in local_profiles}
-        print(local_profiles)
         team_list = []
         for g in global_members:
             profile = local_by_uid.get(g["id"])
