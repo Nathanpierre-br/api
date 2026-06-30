1a1bbf4a3ab994249b4bb62dba4e9e539fb2c1f1
alx0rr <anon.mail.al@proton.me>
2026-06-30 09:44:33 +0300

    test fix search8

--- objects/links.py
+++ objects/links.py
@@ -16,7 +16,7 @@ class Links:
                     "shareURLShortCode": Config.SITE_BASE_URL + "/u/" + data["code"],
                     "targetCode": 1,
                     "ndcId": data.get("ndcId", 0),
-                    "fullPath": f"{loc}/user-profile/{data['objectId']}",
+                    "fullPath": f"ndc://{loc}/user-profile/{data['objectId']}",
                     "shortCode": data.get("code"),
                     "shareURLFullPath": Config.SITE_BASE_URL + "/u/" + data["code"],
                     "objectType": data["objectType"],
@@ -37,7 +37,7 @@ class Links:
                     "shareURLShortCode": Config.SITE_BASE_URL + "/p/" + data["code"],
                     "targetCode": 1,
                     "ndcId": ndcId,
-                    "fullPath": f"{loc}/blog/{data['objectId']}",
+                    "fullPath": f"ndc://{loc}/blog/{data['objectId']}",
                     "shortCode": data["code"],
                     "shareURLFullPath": Config.SITE_BASE_URL + "/p/" + data["code"],
                     "objectType": data["objectType"],
@@ -58,7 +58,7 @@ class Links:
                     "shareURLShortCode": Config.SITE_BASE_URL + "/p/" + data["code"],
                     "targetCode": 1,
                     "ndcId": ndcId,
-                    "fullPath": f"{loc}/chat-thread/{data['objectId']}",
+                    "fullPath": f"ndc://{loc}/chat-thread/{data['objectId']}",
                     "shortCode": data["code"],
                     "shareURLFullPath": Config.SITE_BASE_URL
                     + "/web/x0/chat-thread/"
@@ -74,14 +74,14 @@ class Links:
         ndcId = data.get("ndcId", data.get("objectId", 0))
         loc = "g" if ndcId == 0 else f"x{ndcId}"
         obj = {
-            "path": f"{loc}/community/{data['objectId']}",
+            "path": f"{loc}/community/{ndcId}",
             "extensions": {
                 "linkInfo": {
-                    "objectId": data["objectId"],
+                    "objectId": int(data["objectId"]),
                     "shareURLShortCode": Config.SITE_BASE_URL + "/c/" + data["code"],
                     "targetCode": 1,
                     "ndcId": ndcId,
-                    "fullPath": f"{loc}/community/{ndcId}",
+                    "fullPath": f"ndc://{loc}/community/{ndcId}",
                     "shortCode": data["code"],
                     "shareURLFullPath": Config.SITE_BASE_URL + "/web/x0/community/" + data["objectId"],
                     "objectType": data["objectType"],
